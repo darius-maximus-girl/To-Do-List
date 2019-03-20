e97422b7e4c6feb06e92e5626e09b04c3ac6eb78
darius maximus <dariulqa@wp.pl>
2019-03-20 23:04:56 +0100

    Updates

---
+++ precache-manifest.e950217846026dcc66de6ea93c13842b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2935d60e276d3fc66ed7",
+    "url": "/darius-maximus-girl/To-Do-List/static/js/runtime~main.332536c4.js"
+  },
+  {
+    "revision": "3d2bdb35d6b01f573caa",
+    "url": "/darius-maximus-girl/To-Do-List/static/js/main.d4c0c097.chunk.js"
+  },
+  {
+    "revision": "782431a7a9fd8d41f546",
+    "url": "/darius-maximus-girl/To-Do-List/static/js/2.8b2938bc.chunk.js"
+  },
+  {
+    "revision": "3d2bdb35d6b01f573caa",
+    "url": "/darius-maximus-girl/To-Do-List/static/css/main.e763e88f.chunk.css"
+  },
+  {
+    "revision": "0234cf5e763a7f21d79b6e813bba01cd",
+    "url": "/darius-maximus-girl/To-Do-List/index.html"
+  }
+];
